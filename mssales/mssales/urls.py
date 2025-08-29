09a01421f2f268f28cb18ed0435288df3f656b60
tonybnya <nya.tony2010@gmail.com>
2025-08-29 10:00:55 +0100

    fix: temporary remove the docs endpoint

--- mssales/mssales/urls.py
+++ mssales/mssales/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('apps.products.urls')),
+    path('api/v1/', include('apps.customers.urls')),
+    path('api/v1/', include('apps.sales.urls')),
+    path('api/v1/', include('apps.inventory.urls')),
+    path('api/auth/', include('apps.authentication.urls')),
 ]
